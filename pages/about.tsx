7d605153c520ad89ec92e9340325b134279df71c
Colin <skow0020@gmail.com>
2020-08-31 15:59:19 -0500

    Add initial About stuff and images

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,10 +7,29 @@ const AboutStyle = {
     paddingBottom: "100px"
 } as React.CSSProperties
 
+const MainImageStyle = {
+    maxWidth: "460px"
+}
+
+const AboutInfoStyle = {
+    color: "#c8a97e",
+    fontFamily: "Great Vibes"
+}
+
 const About: React.FC = () => {
     return (
         <Container style={AboutStyle}>
             <PageTitle title="About" />
+            <div>
+                <img
+                    src="images/lillys.jpg"
+                    alt="About Lilly's"
+                    style={MainImageStyle}
+                />
+            </div>
+            <div>
+                <h2 style={AboutInfoStyle}>Delicious, Fulfilling, Healthy Asian Food</h2>
+            </div>
         </Container>
     )
 }
