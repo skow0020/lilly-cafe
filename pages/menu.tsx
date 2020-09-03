7b8b282e514fb07e51ae29dd8ff1236ba29e916f
Colin SKow <skow0020@gmail.com>
2020-09-03 09:56:32 -0500

    Update menu with Appetizers

--- pages/menu.tsx
+++ pages/menu.tsx
@@ -14,12 +14,12 @@ const RowStyle = {
     display: "inline=block"
 }
 
-const Menu: React.FC = ({ beefItems, veggieItems }: any) => {
+const Menu: React.FC = ({ beefItems, veggieItems, appetizers }: any) => {
     return (
         <Container style={MenuStyle}>
             <PageTitle id="menuTitle" title="Menu" />
             <Row xs={1} className="card-deck row-1" style={RowStyle}>
-                <MenuCard id="card-1" type="Beef" menuCardItems={beefItems} />
+                <MenuCard id="card-1" type="Appetizers" menuCardItems={appetizers} />
                 <MenuCard id="card-2" type="Vegetable" menuCardItems={veggieItems} />
                 <MenuCard id="card-3" type="Chicken" menuCardItems={beefItems} />
             </Row>
@@ -42,11 +42,13 @@ const Menu: React.FC = ({ beefItems, veggieItems }: any) => {
 export async function getStaticProps(): Promise<any> {
     const beefItems = getMenuItemsData("beef")
     const veggieItems = getMenuItemsData("veggie")
+    const appetizers = getMenuItemsData("Appetizers")
 
     return {
         props: {
             beefItems,
-            veggieItems
+            veggieItems,
+            appetizers
         }
     }
 }
