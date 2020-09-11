e5fe0cf3a09cabfeba9efc0d39244030890e91cf
Colin SKow <skow0020@gmail.com>
2020-09-11 15:26:39 -0500

    Add some tests using jest and react testing lib

---
+++ tests/data.tsx
@@ -0,0 +1,20 @@
+export const testData = [
+    {
+      id: "chinese_eggrolls",
+      item: "Chinese Egg Rolls (2)",
+      price: "$4.59",
+      description: "Chicken with cabbage and carrot wrapped in egg roll skin"
+    },
+    {
+      id: "cream_cheese_wontons",
+      item: "Cream Cheese Wonton (6)",
+      price: "$4.79",
+      description: "6 yummy cream cheese wontons"
+    },
+    {
+      id: "fried_potstickers",
+      item: "Fried Potstickers (8)",
+      price: "$7.29",
+      description: "Dumplings with pork and cabbage"
+    }
+  ]
---
+++ tests/index.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react"
+import Home from "../pages/index"
+
+describe("Home", () => {
+  it("renders without crashing", () => {
+    render(<Home />)
+    expect(screen.getByRole("heading", { name: "Lilly's Asian Cafe" })).toBeInTheDocument()
+    expect(screen.getByRole("heading", { name: "Call to Order!" })).toBeInTheDocument()
+    expect(screen.getByRole("img", { name: "First slide" })).toBeInTheDocument()
+    expect(screen.getByRole("img", { name: "Second slide" })).toBeInTheDocument()
+    expect(screen.getByRole("img", { name: "Third slide" })).toBeInTheDocument()
+  })
+})
