3be93d53368a66af88d2a9c232e1b03c757ac1ca
Colin <skow0020@gmail.com>
2020-09-22 14:58:37 -0500

    eslint fix

--- lib/items.tsx
+++ lib/items.tsx
@@ -1,8 +1,6 @@
 import fs from "fs"
 import path from "path"
 import matter from "gray-matter"
-import remark from "remark"
-import html from "remark-html"
 
 const menuItemsDirectory = path.join(process.cwd(), "menuItems")
 
